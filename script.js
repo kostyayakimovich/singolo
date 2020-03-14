22c9e207e7e77cf72dc4879c23391efe11f6dd25
kostyayakimovich <yakimovichkostya@gmail.com>
2020-03-14 09:50:08 +0300

    feat: change styles and add functional for portfolio

--- script.js
+++ script.js
@@ -8,7 +8,23 @@ const blackVertical = document.getElementById("blackVertical");
 const blackHorisont = document.getElementById("blackHorisont");
 const imgVertical = document.getElementById("imgVertical");
 const imgHorizontal = document.getElementById("imgHorizontal");
-
+const portfolioContainer = document.getElementById("portfolioContainer");
+const MENUportolio = document.getElementById("menuPortfolio");
+const allPortfolio = document.getElementById("allPortfolio");
+const imgPortfolioBox = [
+  { name: "design1", src: "./assets/img_portfolio/ship.jpg" },
+  { name: "design2", src: "./assets/img_portfolio/princess.jpg" },
+  { name: "design3", src: "./assets/img_portfolio/city.jpg" },
+  { name: "design4", src: "./assets/img_portfolio/robotlove.jpg" },
+  { name: "design5", src: "./assets/img_portfolio/monster_group.jpg" },
+  { name: "design6", src: "./assets/img_portfolio/ide.jpg" },
+  { name: "design7", src: "./assets/img_portfolio/monster_plus.jpg" },
+  { name: "design8", src: "./assets/img_portfolio/birds.jpg" },
+  { name: "design9", src: "./assets/img_portfolio/green_monster.jpg" },
+  { name: "design10", src: "./assets/img_portfolio/video_recorder.jpg" },
+  { name: "design11", src: "./assets/img_portfolio/white_monster.jpg" },
+  { name: "design11", src: "./assets/img_portfolio/degree.jpg" }
+];
 let countClickArrow = 1;
 let countVerticalPhone = 1;
 let countHorizontalPhone = 1;
@@ -52,4 +68,79 @@ blackHorisont.addEventListener("click", event => {
   countHorizontalPhone % 2 !== 0
     ? (blackHorisont.style.display = "none")
     : (blackHorisont.style.display = "inline-block");
+});
+//portfolio img
+
+function paintImg(index) {
+  let imgPortfolio = document.createElement("img");
+  imgPortfolio.className = "img_portfolio active";
+  imgPortfolio.setAttribute("src", imgPortfolioBox[index].src);
+  portfolioContainer.append(imgPortfolio);
+}
+function addOrderImg() {
+  for (let i = 0; i < imgPortfolioBox.length; i++) {
+    paintImg(i);
+  }
+}
+addOrderImg();
+function getRandom(max) {
+  const data = [];
+  return function randomNumber() {
+    const number = Math.floor(Math.random() * Math.floor(max));
+
+    if (data.length >= max) {
+      return;
+    }
+    if (data.includes(number)) {
+      return randomNumber();
+    }
+    data.push(number);
+
+    return number;
+  };
+}
+let boxRandom12 = [];
+
+function buildCard(value) {
+  let getRandomNumber = getRandom(12);
+
+  for (let i = 0; i < value; i++) {
+    let result = getRandomNumber();
+    boxRandom12.push(result);
+  }
+
+  return boxRandom12;
+}
+
+function addImgRandom() {
+  buildCard(12);
+
+  for (let i = 0; i < boxRandom12.length; i++) {
+    paintImg(boxRandom12[i]);
+  }
+}
+function clearImg() {
+  imgPortfolio.remove();
+}
+
+//navigation portfolio
+
+MENUportolio.addEventListener("click", event => {
+  MENUportolio.querySelectorAll("li").forEach(el => {
+    el.classList.remove("active");
+    //clearImg();
+  });
+  event.target.classList.add("active");
+
+  portfolioContainer.innerHTML = "";
+  boxRandom12 = [];
+  addImgRandom();
+});
+
+portfolioContainer.addEventListener("click", event => {
+  portfolioContainer.querySelectorAll("img").forEach(el => {
+    el.classList.remove("active_portfolio_img");
+    //clearImg();
+  });
+  event.target.classList.add("active_portfolio_img");
 });
