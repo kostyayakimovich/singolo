c37409dde4632e5c387b9c879e4781f33101ca9f
kostyayakimovich <yakimovichkostya@gmail.com>
2020-03-16 11:59:31 +0300

    fix: change functional slider and header

--- script.js
+++ script.js
@@ -8,11 +8,24 @@ const slide1 = document.getElementById("slide1");
 const img = document.createElement("img");
 const blackVertical = document.getElementById("blackVertical");
 const blackHorisont = document.getElementById("blackHorisont");
-const imgVertical = document.getElementById("imgVertical");
-const imgHorizontal = document.getElementById("imgHorizontal");
+const imgVerticalSquare = document.getElementById("imgVerticalSquare");
+const imgHorizontalSquare = document.getElementById("imgHorisontalSquare");
 const portfolioContainer = document.getElementById("portfolioContainer");
 const MENUportolio = document.getElementById("menuPortfolio");
 const allPortfolio = document.getElementById("allPortfolio");
+const btnSubmit = document.getElementById("btnSubmit");
+const registration = document.getElementById("registrationBlock");
+const messageBlock = document.createElement("div");
+const message = document.createElement("div");
+const messageHeader = document.createElement("p");
+const messageTopic = document.createElement("p");
+const inputSubject = document.getElementById("inputSubject").value;
+const inputTextarea = document.getElementById("inputTextarea").value;
+const closeBtn = document.createElement("button");
+const messageDescription = document.createElement("p");
+const resultDescription = document.createElement("span");
+const resultSubject = document.createElement("span");
+const containerSlide1 = document.getElementById("containerSlide1");
 const imgPortfolioBox = [
   { name: "design1", src: "./assets/img_portfolio/ship.jpg" },
   { name: "design2", src: "./assets/img_portfolio/princess.jpg" },
@@ -27,12 +40,11 @@ const imgPortfolioBox = [
   { name: "design11", src: "./assets/img_portfolio/white_monster.jpg" },
   { name: "design11", src: "./assets/img_portfolio/degree.jpg" }
 ];
+let boxRandom12 = [];
 let countClickArrow = 0;
-let countClickArrow2 = 0;
 let countVerticalPhone = 1;
 let countHorizontalPhone = 1;
-//const clickLeftArrow;
-//const clickRightArrow;
+
 img.setAttribute("src", "./assets/img_singolo1/Slider.png");
 img.className = "slide2";
 
@@ -44,16 +56,13 @@ MENU.addEventListener("click", event => {
 });
 //slider
 
-const containerSlide2 = document.getElementById("containerSlide2");
-const containerSlide1 = document.getElementById("containerSlide1");
 sliderLeft1.addEventListener("click", event => {
   let interval = setInterval(function() {
     if (containerSlide1.style.left === "-1020px") {
       clearInterval(interval);
-
       return;
     } else {
-      countClickArrow--;
+      countClickArrow -= 20;
       containerSlide1.style.left = `${countClickArrow}px`;
     }
   }, 1);
@@ -62,95 +71,48 @@ sliderRight1.addEventListener("click", event => {
   let interval = setInterval(function() {
     if (containerSlide1.style.left === "1020px") {
       clearInterval(interval);
-
       return;
     } else {
-      countClickArrow++;
+      countClickArrow += 20;
       containerSlide1.style.left = `${countClickArrow}px`;
     }
   }, 1);
 });
 sliderLeft2.addEventListener("click", event => {
   containerSlide1.style.left = "1020px";
-
   countClickArrow = 1020;
   let interval = setInterval(function() {
     if (containerSlide1.style.left === "0px") {
       clearInterval(interval);
-
       return;
     } else {
-      countClickArrow--;
-      containerSlide1.style.left = `${countClickArrow}px`;
-    }
-  }, 1);
-});
-sliderLeft2.addEventListener("click", event => {
-  containerSlide1.style.left = "1020px";
-
-  countClickArrow = 1020;
-  let interval = setInterval(function() {
-    if (containerSlide1.style.left === "0px") {
-      clearInterval(interval);
-
-      return;
-    } else {
-      countClickArrow--;
-      containerSlide1.style.left = `${countClickArrow}px`;
-    }
-  }, 1);
-});
-sliderLeft2.addEventListener("click", event => {
-  containerSlide1.style.left = "1020px";
-
-  countClickArrow = 1020;
-  let interval = setInterval(function() {
-    if (containerSlide1.style.left === "0px") {
-      clearInterval(interval);
-
-      return;
-    } else {
-      countClickArrow--;
+      countClickArrow -= 20;
       containerSlide1.style.left = `${countClickArrow}px`;
     }
   }, 1);
 });
 sliderRight2.addEventListener("click", event => {
   containerSlide1.style.left = "-1020px";
-
   countClickArrow = -1020;
   let interval = setInterval(function() {
     if (containerSlide1.style.left === "0px") {
       clearInterval(interval);
-
       return;
     } else {
-      countClickArrow++;
+      countClickArrow += 20;
       containerSlide1.style.left = `${countClickArrow}px`;
     }
   }, 1);
 });
 
 //black screen
-imgVertical.addEventListener("click", event => {
+imgVerticalSquare.addEventListener("click", event => {
   countVerticalPhone++;
   countVerticalPhone % 2 !== 0
     ? (blackVertical.style.display = "none")
     : (blackVertical.style.display = "inline-block");
 });
-imgHorizontal.addEventListener("click", event => {
-  countHorizontalPhone++;
-  countHorizontalPhone % 2 !== 0
-    ? (blackHorisont.style.display = "none")
-    : (blackHorisont.style.display = "inline-block");
-});
-blackVertical.addEventListener("click", event => {
-  countVerticalPhone++;
-  countVerticalPhone % 2 !== 0
-    ? (blackVertical.style.display = "none")
-    : (blackVertical.style.display = "inline-block");
-});
-blackHorisont.addEventListener("click", event => {
+imgHorisontalSquare.addEventListener("click", count => {
   countHorizontalPhone++;
   countHorizontalPhone % 2 !== 0
     ? (blackHorisont.style.display = "none")
@@ -174,7 +136,6 @@ function getRandom(max) {
   const data = [];
   return function randomNumber() {
     const number = Math.floor(Math.random() * Math.floor(max));
-
     if (data.length >= max) {
       return;
     }
@@ -182,11 +143,9 @@ function getRandom(max) {
       return randomNumber();
     }
     data.push(number);
-
     return number;
   };
 }
-let boxRandom12 = [];
 
 function buildCard(value) {
   let getRandomNumber = getRandom(12);
@@ -201,7 +160,6 @@ function buildCard(value) {
 
 function addImgRandom() {
   buildCard(12);
-
   for (let i = 0; i < boxRandom12.length; i++) {
     paintImg(boxRandom12[i]);
   }
@@ -214,7 +172,6 @@ MENUportolio.addEventListener("click", event => {
     el.classList.remove("active");
   });
   event.target.classList.add("active");
-
   portfolioContainer.innerHTML = "";
   boxRandom12 = [];
   addImgRandom();
@@ -227,54 +184,44 @@ portfolioContainer.addEventListener("click", event => {
   event.target.classList.add("active_portfolio_img");
 });
 // form
-const btnSubmit = document.getElementById("btnSubmit");
 
-const messageBlock = document.createElement("div");
 messageBlock.className = "message_block hidden";
 messageBlock.setAttribute("id", "messageBlock");
-const registration = document.getElementById("registrationBlock");
 registration.append(messageBlock);
-const message = document.createElement("div");
 message.setAttribute("id", "message");
 message.className = "message";
 messageBlock.append(message);
 //header
-const messageHeader = document.createElement("p");
+
 messageHeader.innerText = "Письмо отправлено";
 message.append(messageHeader);
 //topic(subject)
-const messageTopic = document.createElement("p");
+
 messageTopic.innerText = "Без темы";
-const resultSubject = document.createElement("span");
 messageTopic.append(resultSubject);
 message.append(messageTopic);
 
 //textarea description
-const messageDescription = document.createElement("p");
+
 messageDescription.innerText = "Без описания";
-const resultDescription = document.createElement("span");
 messageDescription.append(resultDescription);
 message.append(messageDescription);
 //buttonOK
-const closeBtn = document.createElement("button");
+
 closeBtn.className = "closeBtn input_form";
 closeBtn.innerText = "OK";
 message.append(closeBtn);
-const inputSubject = document.getElementById("inputSubject").value;
-const inputTextarea = document.getElementById("inputTextarea").value;
-
 btnSubmit.onclick = function checkForm() {
   const input = document.getElementById("inputUsername").value;
   const email = document.getElementById("inputEmail").value;
   const inputSubject = document.getElementById("inputSubject").value;
   const inputTextarea = document.getElementById("inputTextarea").value;
-
   if (input && email) {
     messageTopic.innerText = inputSubject
-      ? `Тема: ${inputSubject}`
+      ? `Тема:  ${inputSubject}`
       : "Без темы";
     messageDescription.innerText = inputTextarea
-      ? `Описание: ${inputTextarea}`
+      ? `Описание:  ${inputTextarea}`
       : "Без описания";
   } else {
     return true;
